cf843b14dd9c0143bade455467221c6e80f28698
Iuliu-Antoniu Popovici <66873306+Lewluu@users.noreply.github.com>
2025-10-20 16:18:26 +0300

    Update ci2ci monitoring feature (#7)
    * Add ci2ci monitoring to centralize multiple workflows into one
    
    Signed-off-by: Iuliu-Antoniu Popovici <iuliu-antoniu.popovici@analog.com>
    
    * Undo testing modifications
    
    Signed-off-by: Iuliu-Antoniu Popovici <iuliu-antoniu.popovici@analog.com>
    
    * Undo testing modifications - 2
    
    Signed-off-by: Iuliu-Antoniu Popovici <iuliu-antoniu.popovici@analog.com>
    
    ---------
    
    Signed-off-by: Iuliu-Antoniu Popovici <iuliu-antoniu.popovici@analog.com>

--- CI/app/src/index.ts
+++ CI/app/src/index.ts
@@ -1,4 +1,5 @@
 import process from "process";
+import * as exec from "@actions/exec";
 import { Octokit } from "@octokit/rest";
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
@@ -7,17 +8,23 @@ const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 const [REPO_OWNER, REPO_NAME] = process.env.GITHUB_REPOSITORY!.split("/");
 const GITHUB_RUN_ID = process.env.GITHUB_RUN_ID!;
 const HDL_PROJECT_NAME = process.env.HDL_PROJECT_NAME!;
+const GH_OS_TOKEN = process.env.GH_OS_TOKEN!;
 
 // Method to search for a workflow by name an return the id
-const searchForWorkflowRun = async (searchName: string) => {
+const searchForWorkflowRun = async (workflowId: string, searchName: string) => {
   let runId: number | null = null;
+
+  console.log(`Searching for ${searchName}`);
 
   while (!runId) {
     const runs = await octokit.actions.listWorkflowRuns({
       owner: REPO_OWNER,
       repo: REPO_NAME,
-      workflow_id: "get-response.yml",
+      workflow_id: workflowId,
       event: "workflow_dispatch",
+      headers: {
+        authorization: `token ${GH_OS_TOKEN}`,
+      },
     });
 
     const run = runs.data.workflow_runs.find((r) => r.name === searchName);
@@ -44,69 +51,77 @@ const searchForWorkflowRun = async (searchName: string) => {
 // Method to monitor testing status - TO BE IMPLEMENTED
 
 // Method to monitor build workflow and log its status
-const monitorWorkflowRun = async (searchName: string) => {
+const monitorResponseWorkflow = async (searchName: string) => {
   // Search for triggered workflow run by name
-  const runId = await searchForWorkflowRun(searchName);
+  const runId = await searchForWorkflowRun("get-response.yml", searchName);
 
   // Monitor the workflow run status until it completes
-  let status = "in_progress";
-  while (status === "in_progress") {
+  let status;
+  while (status !== "completed") {
     const { data: runData } = await octokit.actions.getWorkflowRun({
       owner: REPO_OWNER,
       repo: REPO_NAME,
       run_id: runId,
+      headers: {
+        authorization: `token ${GH_OS_TOKEN}`,
+      },
     });
 
     status = runData.status!;
-    console.log(`Workflow run status: ${status}`);
-
-    if (status === "completed") {
-      console.log(
-        `Workflow run completed with conclusion: ${runData.conclusion}`,
-      );
-      if (runData.conclusion !== "success") {
-        throw new Error(
-          `Workflow run failed with conclusion: ${runData.conclusion}`,
-        );
-      }
-      break;
-    }
+    console.log(`Workflow run status for ${searchName}: ${status}`);
 
     await delay(5000); // Wait before checking again
   }
+  console.log(`Workflow run completed with conclusion: ${status}`);
 };
 
 // Method to get data of log build workflow
-const getLogData = async (searchName: string) => {
+const monitorLogWorkflow = async (searchName: string) => {
   // Search for triggered workflow run by name
-  const runId = await searchForWorkflowRun(searchName);
+  const runId = await searchForWorkflowRun("get-log.yml", searchName);
 
   // Fetch the logs for the workflow run
   const logsResponse = await octokit.actions.downloadWorkflowRunLogs({
     owner: REPO_OWNER,
     repo: REPO_NAME,
     run_id: runId,
+    headers: {
+      authorization: `token ${GH_OS_TOKEN}`,
+    },
   });
 
   console.log(`Logs URL: ${logsResponse.url}`);
+
+  // Download log zip file
+  await exec.exec("mkdir", [searchName]);
+  await exec.exec("wget", [
+    "-v",
+    "-O",
+    `${searchName}/logs.zip`,
+    logsResponse.url,
+  ]);
+
+  // Unzip and read log file content
+  await exec.exec("unzip", [`${searchName}/logs.zip`, "-d", searchName]);
+  await exec.exec("cat", [`${searchName}/get-log/2_Get log.txt`]);
 };
 
 // Main starting point
 (async () => {
   try {
     // Check workflow run for build
-    await monitorWorkflowRun(
+    await monitorResponseWorkflow(
       `HDL-Build-for-${HDL_PROJECT_NAME}_${GITHUB_RUN_ID}`,
     );
-    await getLogData(
+    await monitorLogWorkflow(
       `Output-for-HDL-Build-for-${HDL_PROJECT_NAME}_${GITHUB_RUN_ID}`,
     );
 
     // Check workflow run for publish
-    await monitorWorkflowRun(
+    await monitorResponseWorkflow(
       `HDL-Publish-for-${HDL_PROJECT_NAME}_${GITHUB_RUN_ID}`,
     );
-    await getLogData(
+    await monitorLogWorkflow(
       `Output-for-HDL-Publish-for-${HDL_PROJECT_NAME}_${GITHUB_RUN_ID}`,
     );
   } catch (error) {
